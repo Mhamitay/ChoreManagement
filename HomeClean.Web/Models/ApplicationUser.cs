bef2302a02c852b6df90a47c81299152cada3c78
mhamitay <mhamitay@gmail.com>
2022-02-13 20:03:56 -0500

    just created asp.net core / Angular

---
+++ HomeClean.Web/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HomeClean.Web.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
--- homeCleaning/Program.cs
+++ homeCleaning/Program.cs
@@ -1,22 +1,19 @@
 ﻿using homeCleaning.Models;
-using homeCleaning.services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace homeCleaning
 {
-    class Program
+    partial class Program
     {
 
         static void Main(string[] args)
         {
             GameManager gameManager = new GameManager();
-            var playerlst = new FromMemDataRepository().GetAllPlayers();
-            var playersWithNumbers = gameManager.GetRandomNumberForEachPlayer(playerlst);
 
-            var shorelst = new FromMemDataRepository().GetAllShors();
-            var shoresWithNumbers = gameManager.GetRandomNumberForEachshore(shorelst);
+            var playersWithNumbers = gameManager.GetRandomNumberForEachPlayer();
+            var shoresWithNumbers = gameManager.GetRandomNumberForEachshore();
 
             AGAIN:
             Console.Clear();
@@ -71,69 +68,6 @@ namespace homeCleaning
                 Console.WriteLine($" You have selected --- {shore.name}");
                 Console.ForegroundColor = ConsoleColor.White;
             };
-        }
-
-        private class GameManager
-        {
-            public IDictionary<int, string> GetRandomNumberForEachPlayer(List<Player> players)
-            {
-                var usedRandom = new List<int>();
-                var playerList = new Dictionary<int, string>();
-
-                int min = 0;
-                int max = players.Count;
-
-                foreach (var player in players)
-                {
-                lable:
-                    var numb = getRandomNumber(min, max);
-
-                    if (!playerList.TryGetValue(numb, out string _player))
-                    {
-                        playerList.Add(numb, player._name);
-                        usedRandom.Add(numb);
-                    }
-                    else
-                    {
-                        goto lable;
-                    };
-
-                }
-                return playerList;
-            }
-            public IDictionary<int, Shore> GetRandomNumberForEachshore(List<Shore> shores)
-            {
-                var usedRandom = new List<int>();
-                var shoreList = new Dictionary<int, Shore>();
-                int min = 10;
-                int max = shores.Count * 10;
-
-                foreach (var shore in shores)
-                {
-                lable:
-                    var numb = getRandomNumber(min, max);
-
-                    Shore _shore = null;
-                    if (!shoreList.TryGetValue(numb, out _shore))
-                    {
-                        shoreList.Add(numb, shore);
-                        usedRandom.Add(numb);
-                    }
-                    else
-                    {
-                        goto lable;
-
-                    };
-                }
-                return shoreList;
-            }
-            public int getRandomNumber(int min, int max)
-            {
-                Random random = new Random();
-                int rInt = random.Next(min, max + 1);
-
-                return rInt;
-            }
         }
     }
 }
